3f1524696ebf51c8c79fc9f07c61d599e3790520
Tom Ruette <tom@kunsten.be>
2016-11-04 12:28:24 +0100

    organisatie einddatum

--- wikipedia_voor_bart/importdata/query.py
+++ wikipedia_voor_bart/importdata/query.py
@@ -386,7 +386,22 @@ class datakunstenbetriples():
         return DataFrame([[organisatie_id, "oprichtingsdatum", datetime(os[0], os[1], os[2]) if os else "NA"]], columns=["organisatie_id", "relatie", "value"])
 
     def get_organisatie_einddatum(self, organisatie_id):
-        pass
+        sql = """
+        SELECT
+            date_isaars.year,
+            date_isaars.month,
+            date_isaars.day
+        FROM
+            production.organisations
+        INNER JOIN
+            production.date_isaars
+        ON
+            organisations.cancellation_date_id = date_isaars.id
+        WHERE organisations.id = {0}
+        """.format(organisatie_id)
+        self.cur.execute(sql)
+        os = self.cur.fetchone()
+        return DataFrame([[organisatie_id, "einddatum", datetime(os[0], os[1], os[2]) if os else "NA"]], columns=["organisatie_id", "relatie", "value"])
 
     def get_organisatie_activiteitenstart(self, organisatie_id):
         pass
