3a9444e2ab39b32b731e896d8f134fb95d3d530d
Tom Ruette <tom@kunsten.be>
2017-02-15 18:49:23 +0100

    toevoegen van locatiegegevens

--- wikipedia_voor_bart/schrijflijst/query.py
+++ wikipedia_voor_bart/schrijflijst/query.py
@@ -19,6 +19,7 @@ knst_cur = knst.cursor()
 sql = """
 SELECT
   organisations.name,
+  locations.city_nl, countries.name_nl, locations.zip_code,
   count(production.productions.id)
 FROM
   production.organisations,
@@ -26,7 +27,8 @@ FROM
   production.relationships,
   production.seasons,
   production.functions,
-  production.countries
+  production.countries,
+  production.locations
 WHERE
   organisations.id = relationships.organisation_id AND
   organisations.country_id = countries.id AND
@@ -34,10 +36,12 @@ WHERE
   relationships.production_id = productions.id AND
   functions.id = relationships.function_id AND
   seasons.start_year >= 1993 AND
+  organisations.location_id = locations.id AND
+  countries.id = locations.country_id AND
   functions.name_nl IN ('gezelschap', 'producent') AND
   countries.name_en = 'Belgium'
 GROUP BY
-  production.organisations.name
+  production.organisations.name, locations.city_nl, countries.name_nl, locations.zip_code
 """
 
 lijst_van_organisaties = []
@@ -45,7 +49,7 @@ knst_cur.execute(sql)
 for row in knst_cur.fetchall():
     lijst_van_organisaties.append(list(row))
 
-DataFrame(lijst_van_organisaties, columns=["Lijst van organisaties", "Count van producties"]).to_csv("schrijflijst/lijst_van_organisaties.csv")
+DataFrame(lijst_van_organisaties, columns=["Lijst van organisaties", "Stad", "Land", "Postcode", "Count van producties"]).to_csv("../schrijflijst/lijst_van_organisaties.csv")
 
 # lijst van alle personen die aan deze producties (1) verbonden zijn
 
@@ -79,19 +83,23 @@ for i, productionid in enumerate(producties):
         print(i, "of", len(producties))
     sql = """
     SELECT DISTINCT
-      people.id, people.full_name, gender_id
+      people.id, people.full_name, gender_id, locations.city_nl, countries.name_nl, locations.zip_code
     FROM
       production.productions,
       production.relationships,
-      production.people
+      production.people,
+      production.locations,
+      production.countries
     WHERE
       relationships.production_id = productions.id AND
       people.id = relationships.person_id AND
+      locations.id = people.location_id AND
+      countries.id = locations.country_id AND
       productions.id = {0}
     """.format(productionid[0])
     knst_cur.execute(sql)
     people_in_production = knst_cur.fetchall()
-    for uid, name, gender in people_in_production:
+    for uid, name, gender, stad, land, zip in people_in_production:
             sql = """
             SELECT DISTINCT COUNT(relationships.production_id)
             FROM production.relationships
@@ -99,6 +107,6 @@ for i, productionid in enumerate(producties):
             """.format(uid)
             knst_cur.execute(sql)
             c = knst_cur.fetchone()[0]
-            personen.add((name, gender, c))
+            personen.add((name, gender, stad, land, zip, c))
 
-DataFrame(list(personen), columns=["Lijst van personen", "Gender", "Count van producties"]).to_csv("lijst_van_personen.csv")
+DataFrame(list(personen), columns=["Lijst van personen", "Gender", "Stad", "Land", "Postcode", "Count van producties"]).to_csv("lijst_van_personen.csv")
